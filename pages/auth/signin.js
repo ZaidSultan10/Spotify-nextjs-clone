3663d57d012121f616f5e4710f32e3c7248f54fc
Zaid Sultan <zaidsultan09@gmail.com>
2022-01-01 19:04:37 +0400

    spotify login screen

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
 
 const signin = () => {
     return (
         <div>
-            hello
+            <Head>
+                <title>Spotify - Login</title>
+                <link rel='icon' href='spotify-logo.png'/>
+            </Head>
+            <Image
+            src='https://rb.gy/y9mwtb'
+            height={250}
+            width={600}
+            objectFit='contain'
+            className='animate-pulse' />
         </div>
     )
 }
